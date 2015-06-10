0873c5ea26d3c1f75bd3dc0e4e900e5e3cc2a686
Luke Valentine <lucifargundam@users.noreply.github.com>
2015-06-09 23:23:16 -0700

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -2,5 +2,14 @@
 
 main ()
 {
+  double tp = (p / q);
+  double p, q;
 printf ("Hello and welcome to PriceCalculator");
+// *******************************************//
+printf ("Please Enter Price");
+// **************************************** //
+printf ("Please Enter Quantity");
+// **************************************** //
+printf ("The price per &meas is " + tp);
+// *******====>>>>>Reset Button Here<<<<<=======******* //
 }
