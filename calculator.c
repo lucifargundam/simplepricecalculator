2c1b4e51ecd53e21cdad056b60f0fd7b5ec85a36
Luke Valentine <lucifargundam@users.noreply.github.com>
2015-06-09 23:54:04 -0700

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -2,14 +2,21 @@
 
 main ()
 {
-  double tp = (p / q);
+  char c;
   double p, q;
-printf ("Hello and welcome to PriceCalculator");
-// *******************************************//
-printf ("Please Enter Price");
+  double tp = (p / q);
+
+  printf ("Hello and welcome to PriceCalculator\n");
+// ******************************************* //
+printf ("Please choose abbreviated measurement type:\n");
+scanf ("%4s", c);
+// ******************************************** //
+printf ("Please Enter Price:\n");
+scanf ("%g", p);
 // **************************************** //
-printf ("Please Enter Quantity");
+printf ("Please Enter Quantity:\n");
+scanf ("%g", q);
 // **************************************** //
-printf ("The price per &meas is " + tp);
+printf ("The price per &meas is %g %c\n", tp, c);
 // *******====>>>>>Reset Button Here<<<<<=======******* //
 }
