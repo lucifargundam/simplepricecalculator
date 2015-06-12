f1feba094375a55ee3b39e98efea04d29049e095
Luke Valentine <lucifargundam@users.noreply.github.com>
2015-06-12 01:37:53 -0700

    Update calculator.c
    final string update complete. tomarrow will be the reseet/end loop and then it will be done. would have finished this sooner- but very little time is available to get such things done after my big move to new city along with no money in bank.

--- calculator.c
+++ calculator.c
@@ -3,12 +3,12 @@
 main ()
 {
   float p, q;
-  char meas;
+  char meas[2] = {};
 
 printf ("Hello and welcome to PriceCalculator\n\n\n");
 // ******************************************* //
 printf ("Please choose abbreviated measurement type:\n");
-scanf ("%2c", &meas);
+scanf ("%s", &meas);
 // ******************************************** //
 printf ("\nPlease Enter Price:\n");
 scanf ("%f", &p);
@@ -17,6 +17,6 @@ printf ("\nPlease Enter Quantity:\n");
 scanf ("%f", &q);
 // **************************************** //
   float tp = (p / q);
-  printf ("\nThe price is %f per %c \n\n", tp, meas);
+  printf ("\nThe price is %f per %s \n\n", tp, meas);
 // *******====>>>>>Reset Button Here<<<<<=======******* //
 }
