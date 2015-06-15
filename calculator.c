37addc32561359a09507a84956eaa32094274770
Luke Valentine <lucifargundam@users.noreply.github.com>
2015-06-15 01:20:13 -0700

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -4,9 +4,12 @@ main ()
 {
   float p, q;
   char meas[2] = {};
-
+  char ans = 'n';
+  
 printf ("Hello and welcome to PriceCalculator\n\n\n");
 // ******************************************* //
+For ( ans == 'y');
+// ***888 //
 printf ("Please choose abbreviated measurement type:\n");
 scanf ("%s", &meas);
 // ******************************************** //
@@ -19,4 +22,7 @@ scanf ("%f", &q);
   float tp = (p / q);
   printf ("\nThe price is %f per %s \n\n", tp, meas);
 // *******====>>>>>Reset Button Here<<<<<=======******* //
+printf ("Would you like to enter anouther entry? (y\n)")
+scanf ("%c", &ans);
+Else (break);
 }
